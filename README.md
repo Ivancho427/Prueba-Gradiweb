aafa599d2729938296b625f51fa8942e5c5c3a3e
Ivan Dario Mejia <ivanmm427@gmail.com>
2020-11-05 21:43:41 -0500

    Actualizando cambios finales

---
+++ README.md
@@ -0,0 +1,13 @@
+This aplication is a simple application, built with vanilla javascript.
+
+If you want to download the project and see in your local development machine follow the next steaps.
+
+1) Init Git.
+2) Clone the repo. For example = git clone https://github.com/Ivancho427/Prueba-Gradiweb
+3) Run in the editor, could Visual Studio Code.
+4) Run  whith the extensión "Live Server".
+5) Visit http://localhost:3000
+
+You will see the web application deployed.
+
+Or if you wish you can see in the link:
--- js/main.js
+++ js/main.js
@@ -33,6 +33,7 @@ fetch(
   
  let nombrePar = document.getElementById("name-par");
  let temperaturaPar = document.getElementById("temp-par");
+ let humedadPar = document.getElementById("hum-par")
  let descripcionPar = document.getElementById("descrip-par");
  let iconosPar = document.getElementById("ico-par");
 
@@ -43,12 +44,14 @@ fetch(
     .then((data) => {
       let namePar = data.name;
       nombrePar.innerHTML = `${namePar.toUpperCase()}`;
-
-      
   
       let temperaturePar = data.main.temp;
       temperaturePar = temperaturePar - 273.15;
       temperaturaPar.innerHTML = `Temperatura Actual: ${temperaturePar.toFixed()} °C`;
+
+      let humididyPar = data.main.humidity;
+      console.log(humididyPar);
+      humedadPar.innerHTML = `Humedad: ${humididyPar} %`;
   
       let descriptionPar = data.weather[0].description;
       descripcionPar.innerHTML = descriptionPar;
@@ -70,7 +73,7 @@ fetch(
   )
     .then((response) => response.json())
     .then((data) => {
-    let dia1 = data.list[7].dt_txt;
+    let dia1 = data.list[5].dt_txt;
       // console.log(dia1);
       moment.locale('es');
       let dateTime = moment(dia1);
@@ -78,18 +81,18 @@ fetch(
     //   console.log(newDia1);  
       fechaDia1.innerHTML = `${newDia1.toUpperCase()}`;
   
-      let dia1TempMin = data.list[5].main.temp_min;
+      let dia1TempMin = data.list[3].main.temp_min;
       dia1TempMin = dia1TempMin - 273.15;
       tempMinDia1.innerHTML = `${dia1TempMin.toFixed()} °C`;
   
-      let dia1TempMax = data.list[7].main.temp_max;
+      let dia1TempMax = data.list[5].main.temp_max;
       dia1TempMax = dia1TempMax - 273.15;
       tempMaxDia1.innerHTML = ` ${dia1TempMax.toFixed()} °C`;
   
-      let dia1Description = data.list[7].weather[0].description;
+      let dia1Description = data.list[5].weather[0].description;
       descripcionDia1.innerHTML = dia1Description;
   
-      let dia1Icons = data.list[3].weather[0].icon;
+      let dia1Icons = data.list[5].weather[0].icon;
       iconosDia1.innerHTML = `<img src="https://openweathermap.org/img/wn/${dia1Icons}@2x.png"/>`;
     });
   
@@ -106,24 +109,24 @@ fetch(
   )
     .then((response) => response.json())
     .then((data) => {
-      let dia2 = data.list[15].dt_txt;    
+      let dia2 = data.list[13].dt_txt;    
       moment.locale('es');
       let dateTime = moment(dia2);
       let newDia2 = dateTime.format('dddd');
       fechaDia2.innerHTML = `${newDia2.toUpperCase()}`;
 
-      let dia2TempMin = data.list[13].main.temp_min;
+      let dia2TempMin = data.list[11].main.temp_min;
       dia2TempMin = dia2TempMin - 273.15;
       tempMinDia2.innerHTML = ` ${dia2TempMin.toFixed()} °C`;
   
-      let dia2TempMax = data.list[15].main.temp_max;
+      let dia2TempMax = data.list[13].main.temp_max;
       dia2TempMax = dia2TempMax - 273.15;
       tempMaxDia2.innerHTML = ` ${dia2TempMax.toFixed()} °C`;
     
-      let dia2Description = data.list[15].weather[0].description;
+      let dia2Description = data.list[13].weather[0].description;
       descripcionDia2.innerHTML = dia2Description;
   
-      let dia2Icons = data.list[15].weather[0].icon;
+      let dia2Icons = data.list[13].weather[0].icon;
       iconosDia2.innerHTML = `<img src="https://openweathermap.org/img/wn/${dia2Icons}@2x.png"/>`;
     });
 
@@ -140,23 +143,23 @@ fetch(
   )
     .then((response) => response.json())
     .then((data) => {
-      let dia3 = data.list[23].dt_txt;
+      let dia3 = data.list[21].dt_txt;
       moment.locale('es');
       let dateTime = moment(dia3);
       let newDia3 = dateTime.format('dddd');
       fechaDia3.innerHTML = `${newDia3.toUpperCase()}`;
 
-      let dia3TempMin = data.list[21].main.temp_min;
+      let dia3TempMin = data.list[19].main.temp_min;
       dia3TempMin = dia3TempMin - 273.15;
       tempMinDia3.innerHTML = ` ${dia3TempMin.toFixed()} °C`;
   
-      let dia3TempMax = data.list[23].main.temp_max;
+      let dia3TempMax = data.list[21].main.temp_max;
       dia3TempMax = dia3TempMax - 273.15;
       tempMaxDia3.innerHTML = `${dia3TempMax.toFixed()} °C`;
     
-      let dia3Description = data.list[23].weather[0].description;
+      let dia3Description = data.list[21].weather[0].description;
       descripcionDia3.innerHTML = dia3Description;
   
-      let dia3Icons = data.list[23].weather[0].icon;
+      let dia3Icons = data.list[21].weather[0].icon;
       iconosDia3.innerHTML = `<img src="https://openweathermap.org/img/wn/${dia3Icons}@2x.png"/>`;
     });
